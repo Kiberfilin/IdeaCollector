2523ae65ebf9d8028226b61fb1294f9d86f54251
Kiberfilin <kiberfilin@gmail.com>
2024-10-01 22:03:34 +0300

    настраиваем навигацию

--- modules/features/home_screen/home_screen_impl/src/main/kotlin/com/example/home_screen_impl/HomeScreenFragment.kt
+++ modules/features/home_screen/home_screen_impl/src/main/kotlin/com/example/home_screen_impl/HomeScreenFragment.kt
@@ -4,9 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.core_api.contracts.AppWithFacade
+import com.example.home_screen_impl.di.HomeScreenComponent
+import com.example.settings_api.SettingsScreenMediator
+import javax.inject.Inject
 
 class HomeScreenFragment : Fragment() {
+    @Inject
+    lateinit var settingsScreenMediator: SettingsScreenMediator
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        HomeScreenComponent.create((requireActivity().application as AppWithFacade).getFacade())
+            .inject(this)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,6 +30,12 @@ class HomeScreenFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home_screen, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        view.findViewById<TextView>(R.id.textView).setOnClickListener {
+            settingsScreenMediator.openSettingsScreen(findNavController())
+        }
+        super.onViewCreated(view, savedInstanceState)
+    }
     companion object {
         fun newInstance() = HomeScreenFragment()
     }
--- modules/features/home_screen/home_screen_impl/src/main/kotlin/com/example/home_screen_impl/di/HomeScreenComponent.kt
+++ modules/features/home_screen/home_screen_impl/src/main/kotlin/com/example/home_screen_impl/di/HomeScreenComponent.kt
@@ -1,23 +1,24 @@
 package com.example.home_screen_impl.di
 
 import com.example.core_api.contracts.ProvidersFacade
+import com.example.core_api.di.scope.FragmentScope
 import com.example.home_screen_impl.HomeScreenFragment
 import dagger.Component
-import javax.inject.Singleton
 
-@Singleton
+@FragmentScope
 @Component(
     modules = [HomeScreenModule::class],
     dependencies = [ProvidersFacade::class]
 )
 interface HomeScreenComponent {
 
-    companion object{
+    companion object {
         fun create(providersFacade: ProvidersFacade) =
             DaggerHomeScreenComponent
                 .builder()
                 .providersFacade(providersFacade)
                 .build()
     }
+
     fun inject(homeScreenFragment: HomeScreenFragment)
 }
